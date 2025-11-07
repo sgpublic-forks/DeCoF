807760140d6dadfd917a12a23407d6305cbbe561
Haven Madray <sgpublic2002@gmail.com>
2025-11-07 13:15:46 +0800

    finish dataset generation

--- src/create_datset.py
+++ src/create_datset.py
@@ -1,22 +1,22 @@
 import argparse
-import os
+from pathlib import Path
 
 from utils.funcs import load_json
-from utils.split import get_split
+from utils.split import get_split, split_video
 
-ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../')
+ROOT = Path(__file__).resolve().parent / '..'
 
-fake_sets = load_json(os.path.join(ROOT, "./download/fake/raw_videos/trains.json"))
+fake_sets = load_json(str(ROOT / "./download/fake/raw_videos/trains.json"))
 
 video_dict = {}
 
 def load_real_video_dict(source):
-    for item in load_json(os.path.join(ROOT, f"./download/real/{source}.json")):
-        if item.video_id not in video_dict:
-            video_dict[item.video_id] = {
-                'source': item.source,
+    for item in load_json(str(ROOT / f"./download/real/{source}.json")):
+        if item['video_id'] not in video_dict:
+            video_dict[item['video_id']] = {
+                'source': item['source'],
             }
-        video_dict[item.video_id]['real'] = os.path.join(ROOT, f"./download/real/{item.source}/raw_videos/{item.video}")
+        video_dict[item['video_id']]['real'] = str(ROOT / f"./download/real/{item['source']}/raw_videos/{item['video']}")
 load_real_video_dict("MSVD/msvd_train")
 load_real_video_dict("MSVD/msvd_test")
 load_real_video_dict("MSVD/msvd_val")
@@ -24,20 +24,35 @@ load_real_video_dict("MSR-VTT/msrvtt_test_1k")
 load_real_video_dict("MSR-VTT/msrvtt_train_9k")
 
 def load_fake_video_dict(source):
-    for item in load_json(os.path.join(ROOT, f"./download/fake/{source}.json")):
-        video_dict[item.video_id]['fake'] = os.path.join(ROOT, f'./download/fake/{video_dict[item.video_id]['source']}/raw_videos/{item.video}')
+    for item in load_json(str(ROOT / f"./download/fake/{source}.json")):
+        if 'fake' not in video_dict[item['video_id']]:
+            video_dict[item['video_id']]['fake'] = {}
+        video_dict[item['video_id']]['fake'][item['source']] = str(ROOT / f'./download/fake/raw_videos/{item['video']}')
 load_fake_video_dict("gvf_fakes_train")
 
 
-def create_dataset_by_phase(phase):
-    video_list = get_split(phase)
+DATASET_ROOT = ROOT / "data"
+def create_dataset(subdatasets_name):
+    dataset_path = DATASET_ROOT / subdatasets_name
+    for phase in ['train', 'val', 'test']:
+        create_dataset_for_phase(subdatasets_name, phase, dataset_path)
 
+def create_dataset_for_phase(subdatasets_name, phase, save_to):
+    video_list = get_split(phase)
+    for video_id in video_list:
+        if video_id not in video_dict:
+            print(f'{video_id} not found video_dict')
+            continue
+        video = video_dict[video_id]
+        split_video(video['real'], save_to / phase / '0_real' / video_id)
+        split_video(video['fake'][subdatasets_name], save_to / phase / '1_fake' / video_id)
 
 
 def main(args):
-    dataset_config = load_json(args.config)
+    dataset_config = load_json(str(ROOT / f'./src/configs/{args.config}.json'))
     subdatasets_name = dataset_config['subdatasets_name']
     assert subdatasets_name in fake_sets
+    create_dataset(subdatasets_name)
 
 
 if __name__=='__main__':
--- src/utils/split.py
+++ src/utils/split.py
@@ -1,10 +1,23 @@
-import os
+from os import path
+from pathlib import Path
+
+import ffmpeg
+
 
 def get_split(phase):
     assert phase in ['train', 'val', 'test']
-    with open(os.path.join(os.path.realpath(__file__), f'../../datas/split/{phase}.json'), 'r') as f:
-        return f.readlines()
+    with open(path.join(path.dirname(path.realpath(__file__)), f'../../datas/split/{phase}.json'), 'r') as f:
+        return [item.strip() for item in f.readlines()]
 
 # ffmpeg -i input_video.mp4 -vf "select='not(mod(n\,4))',setpts=N/FRAME_RATE/TB" -vsync vfr output_%03d.png
-def split_video(video_path, save_path, range=32, count=8):
-    pass
+def split_video(video_path: str, save_path: Path, filenames='%03d.jpg', range=32, count=8):
+    print(f'processing {video_path}')
+    save_path.mkdir(parents=True, exist_ok=True)
+    stream = (
+        ffmpeg
+        .input(video_path)
+        .filter('select', f'lte(n,{range - 1})')     # 只取前32帧
+        .filter('select', f'not(mod(n,{range / count}))')  # 每隔4帧取1帧
+        .output(path.join(save_path, filenames), vsync='0')
+    )
+    ffmpeg.run(stream, overwrite_output=True, quiet=True)
